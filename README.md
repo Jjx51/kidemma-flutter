d23a38b82cd1cb620a3e90e99cce4e81aa114188
Vicente Guzman <vicente.g.guzman@accenture.com>
2025-09-19 12:39:12 -0600

    Creación de Proyecto Base - Hello World

---
+++ README.md
@@ -0,0 +1,3 @@
+# kidemma_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/kidemma_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kidemma_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
