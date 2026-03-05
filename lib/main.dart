666358f94883f6a01678a75f4301fcfbad954f1a
Vicente Guzmán <vicente.g.guzman@accenture.com>
2026-03-05 09:43:30 -0600

    Se agrego SplashScreen
    https://pub.dev/packages/flutter_native_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(const Duration(seconds: 2)); // hold the native splash
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
